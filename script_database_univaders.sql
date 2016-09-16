29a37f287623caa95e5abf585b02ce3a5aac62fb
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2016-09-15 23:42:12 -0300

    Added a file with the database script

---
+++ script_database_univaders.sql
@@ -0,0 +1,19 @@
+﻿
+CREATE DATABASE "Univaders";
+
+
+CREATE TABLE winners
+(
+  cod integer NOT NULL DEFAULT nextval('seq_winner_id'),
+  score integer NOT NULL,
+  name character varying(30) NOT NULL,
+-- identification field can be the school's name, course name, the city, etc.
+  identification character varying(50),
+  date date,
+  CONSTRAINT pk_winners PRIMARY KEY (cod)
+);
+
+
+
+CREATE SEQUENCE seq_winner_id INCREMENT 1;
+
